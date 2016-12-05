ab25e40c94b759f4e1e24e80ead9ab84b901513b
cassidyb <cassidybollin32@gmail.com>
2016-12-05 13:09:06 -0800

    cleaning up

---
+++ js/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        anchors: ['firstPage', 'secondPage', 'thirdPage', 'forthPage'],
+        sectionsColor: ['#E0EBE8', '#48A9A6', '#C1666B', '#C1666B'],
+        css3: true
+    });
+});
