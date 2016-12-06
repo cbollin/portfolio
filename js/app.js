cf9147aec1013519f815006f5defd721b0d7a8df
cassidyb <cassidybollin32@gmail.com>
2016-12-06 15:04:55 -0800

    lots of changes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
-        anchors: ['firstPage', 'secondPage', 'thirdPage', 'forthPage'],
-        sectionsColor: ['#E0EBE8', '#48A9A6', '#C1666B', '#C1666B'],
-        css3: true
+        anchors: ['Home', 'Skills', 'Projects', 'Timeline'],
+        sectionsColor: ['#E0EBE8', '#48A9A6', '#C1666B', '#98C183'],
+        navigation: true,
+        navigationPosition: 'right',
+        navigationTooltips: ['Home', 'Skills', 'Projects', 'Timeline']
     });
 });
