e4fae7b0b7a5fe5c505d65fca227f9f75b410a9a
cassidyb <cassidybollin32@gmail.com>
2016-12-06 19:36:08 -0800

    bootstrap and more

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $('#fullpage').fullpage({
         anchors: ['Home', 'Skills', 'Projects', 'Timeline'],
         sectionsColor: ['#E0EBE8', '#48A9A6', '#C1666B', '#98C183'],
+        scrollOverflow: true,
         navigation: true,
         navigationPosition: 'right',
         navigationTooltips: ['Home', 'Skills', 'Projects', 'Timeline']
