39b910ad680c33b00cf472a3415383c31ec35d99
cassidyb <cassidybollin32@gmail.com>
2016-12-12 17:14:53 -0800

    maybe final commit

--- js/app.js
+++ js/app.js
@@ -1,31 +1,21 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
-        anchors: ['Home', 'Skills', 'Projects', 'Timeline'],
-        sectionsColor: ['#E0EBE8', '#48A9A6', '#C1666B', '#98C183'],
+        anchors: ['Home', 'Skills', 'Projects', 'Timeline', 'Contact'],
+        sectionsColor: ['#E0EBE8', '#48A9A6', '#a3d4d2', '#819BD1', '#333'],
         scrollOverflow: true,
         navigation: true,
         navigationPosition: 'right',
         navigationTooltips: ['Home', 'Skills', 'Projects', 'Timeline']
     });
-    $('[data-toggle="tooltip"]').tooltip();
+    $("i").hover(
+        function() {
+            $(this).stop().animate({
+                "opacity": ".5"
+            }, "fast");
+        },
+        function() {
+            $(this).stop().animate({
+                "opacity": "1"
+            }, "slow");
+        });
 });
-$("i").hover(
-    function() {
-        $(this).stop().animate({
-            "opacity": ".5"
-        }, "fast");
-    },
-    function() {
-        $(this).stop().animate({
-            "opacity": "1"
-        }, "slow");
-    });
-$(".gif").hover(
-    function() {
-        var src = $(this).attr("src");
-        $(this).attr("src", src.replace(/\.png$/i, ".gif"));
-    },
-    function() {
-        var src = $(this).attr("src");
-        $(this).attr("src", src.replace(/\.gif$/i, ".png"));
-    });
